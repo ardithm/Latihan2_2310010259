4fbc8528d08e8e79b2809a531ee3ec39be339060
ardithm <ardianto2300@gmail.com>
2025-06-10 13:34:29 +0800

    initial commit

---
+++ src/latihan2/DataDiri.java
@@ -0,0 +1,11 @@
+package latihan2;
+
+public class DataDiri {
+    private String nama, npm, kelas;
+    
+    DataDiri(String nama, String npm, String kelas){
+        this.nama = nama;
+        this.npm = npm;
+        this.kelas = kelas;
+    }
+}
